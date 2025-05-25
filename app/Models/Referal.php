a9b2c3b90c15a3f591034e8f8e3d4fe12445b2bc
ayoubwazane <ayoubwazane306@gmail.com>
2025-05-25 23:54:35 +0100

    initializing referals

---
+++ app/Models/Referal.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Referal extends Model
+{
+    protected $fillable = [
+        "nom_ref",
+        "date_expiration",
+    ];
+
+}
